e5f7e172b7b7db79582340cbecf5bd627585d6c1
Mirzoyoqub Rashidov <97793909+mirzoyoqubcodes@users.noreply.github.com>
2024-04-20 21:51:51 +0500

    added navbar and header

---
+++ sharpist/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Categories from "./components/Categories/Categories";
+import Business from "./components/Business/Business";
+import Teach from "./components/Teach/Teach";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/business" element={<Business />} />
+      <Route path="/teach" element={<Teach />} />
+    </Routes>
+  );
+}
+
+export default App;
